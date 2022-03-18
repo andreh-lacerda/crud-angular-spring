b3f3fde3170c2b6baad158919485c4634f6cb7e2
André Lacerda <66031421+andreh-lacerda@users.noreply.github.com>
2022-03-18 10:47:37 -0300

    Connect API Spring with Angular

---
+++ crud-spring/src/main/java/com/andre/crudspring/controller/CourseController.java
@@ -0,0 +1,26 @@
+package com.andre.crudspring.controller;
+
+import java.util.List;
+
+import com.andre.crudspring.model.Course;
+import com.andre.crudspring.repository.CourseRepository;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/api/courses")
+@AllArgsConstructor
+public class CourseController {
+
+    private final CourseRepository courseRepository;
+
+    @GetMapping
+    public @ResponseBody List<Course> list() {
+        return courseRepository.findAll();
+    }
+}
